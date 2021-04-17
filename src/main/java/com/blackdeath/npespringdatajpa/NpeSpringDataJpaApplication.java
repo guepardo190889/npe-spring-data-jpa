cbe62525e64c5d22d8430071ed7f5075d307c4f1
Seth Karim Luis Martínez <ing.seth.luis@gmail.com>
2021-04-17 13:34:08 -0500

    Primer commit

---
+++ src/main/java/com/blackdeath/npespringdatajpa/NpeSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.blackdeath.npespringdatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpeSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpeSpringDataJpaApplication.class, args);
+	}
+
+}
